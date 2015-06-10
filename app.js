4d5a20e904c7efd5170829f640a7d8ae1ab682d5
Satomi <satomi@dcl.cs.waseda.ac.jp>
2015-06-10 19:08:40 +0900

    applied oauth-authentication and thus moved some files and changed path

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
+'use strict';
+var http = require('http');
+var express = require('express');
+// passport-twitter用
+var session = require('express-session')
+var auth = require('./passport');
+var passport = auth.passport;
+// ルーティングファイルを指定
+var routes = require('./routes');
+var app = express();
+var server = http.createServer(app);
+
+var conf = require('config');
+
+// passport-twitter用
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+app.use(session({secret: 'itagaki'}));
+
+// 静的ファイルのルートディレクトリを指定
+app.use(express.static(__dirname + '/public'));
+
+// ルーティングを設定
+routes.configRoutes(app, server, passport);
+
+// リッスン
+server.listen(8080);
+console.log('Listening on port %d in %s mode', server.address().port, app.settings.env);
+
+
+/*
+
 // 1.モジュールオブジェクトの初期化
 var fs = require("fs");
-var server = require("http").createServer(function(req, res) {
+var server = require("express").createServer(function(req, res) {
 
     if(req.url.indexOf('.html') != -1){ //req.url has the pathname, check if it conatins '.html'
       fs.readFile(__dirname + 'index.html', function (err, data) {
@@ -31,6 +63,8 @@ var server = require("http").createServer(function(req, res) {
     }    
 
 }).listen(8080);
+
+*/
 
 var io = require("socket.io").listen(server);
 
